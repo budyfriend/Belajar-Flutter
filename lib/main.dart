c890870d0f435f54bfec9dfc3821d746dba071d7
budyfriend <budyfriend764@gmail.com>
2020-11-25 16:47:05 +0700

    Timer

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'dart:async';
 import 'dart:math';
 import 'dart:ui';
 
@@ -28,7 +29,93 @@ import 'package:provider/provider.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 void main() {
-  runApp(LatihanDocComment());
+  runApp(LatihanTimer());
+}
+
+class LatihanTimer extends StatefulWidget {
+  @override
+  _LatihanTimerState createState() => _LatihanTimerState();
+}
+
+class _LatihanTimerState extends State<LatihanTimer> {
+  bool isStop = true;
+  bool isBlack = true;
+  int counter = 0;
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Timer Demo'),
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                counter.toString(),
+                style: TextStyle(
+                    color: (isBlack) ? Colors.black : Colors.red,
+                    fontSize: 40,
+                    fontWeight: FontWeight.bold),
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              RaisedButton(
+                onPressed: () {
+                  Timer(Duration(seconds: 5), () {
+                    setState(() {
+                      isBlack = !isBlack;
+                    });
+                  });
+                },
+                child: Text('Ubah warna 5 detik kemudian'),
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              RaisedButton(
+                onPressed: () {
+                  Timer.run(() {
+                    setState(() {
+                      isBlack = !isBlack;
+                    });
+                  });
+                },
+                child: Text('Ubah warna secara langsung'),
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              RaisedButton(
+                onPressed: () {
+                  counter = 0;
+                  isStop = false;
+                  Timer.periodic(Duration(seconds: 1), (timer) {
+                    if (isStop) timer.cancel();
+                    setState(() {
+                      counter++;
+                    });
+                  });
+                },
+                child: Text('Start timer'),
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              RaisedButton(
+                onPressed: () {
+                  isStop = true;
+                },
+                child: Text('Stop timer'),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
 }
 
 class LatihanDocComment extends StatelessWidget {
